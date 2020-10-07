aaaa528576b69512c094b95ca47dbdc2ad3bdb5b
asimhyff <asimhyff@users.noreply.github.com>
2020-10-07 15:42:15 +0500

    babel added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
 import '../css/style.css'
 import {people} from './bar'
-// const bar = require('./bar')
 
 const data = document.getElementById('root')
 
 data.innerHTML = '<h1>' + people.name + '</h1>'
 
-// console.log(JSON.stringify(people , null , 2))
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,14 @@ module.exports = {
     },
     module:{
         rules:[
-            {test: /\.css$/,loader: "style-loader!css-loader"}
+            {
+                test: /\.css$/,loader: "style-loader!css-loader"
+            },
+            {
+                test: /\.js$/ , loader: "babel-loader" , exclude: /node-modules/ , query:{
+                    preset:['es2015']
+                }
+            }
         ]
     }
 }
